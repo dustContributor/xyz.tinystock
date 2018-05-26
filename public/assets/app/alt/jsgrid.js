464b6170a09a297b0b2bf915240f2ca24549bb03
dustContributor <hctf90@gmail.com>
2018-05-26 00:49:34 -0300

    - Added a convenience getJsGrid method. - Replaced all double quotes with single ones. - Added tooltips to default buttons.

--- public/assets/app/alt/jsgrid.js
+++ public/assets/app/alt/jsgrid.js
@@ -1,9 +1,13 @@
 (() => {
+  /* We add a convenience getJsGrid method to all jQuery objects, like Kendo's. */
+  $.fn['getJsGrid'] = function() {
+    return this.data('JSGrid')
+  }
   /*
    * This is to add a counterpart to 'rowByItem'
    */
   jsGrid.Grid.prototype.itemByRow = function(element) {
-    return element ? (element.jquery ? element : $(element)).data("JSGridItem") : null
+    return element ? (element.jquery ? element : $(element)).data('JSGridItem') : null
   }
   /*
    * This here is to allow to specify Kendo buttons for various of the jsGrid's actions.
@@ -20,27 +24,28 @@
           clickHandler(grid, e.event, e)
         }
       }, cls)
-      return $("<div>").kendoButton(opts)
+      return $('<div title="' + (opts.title || '') + '" >').kendoButton(opts)
     }
     return _createGridButton.call(this, cls, tooltip, clickHandler)
   }
   const kendoButtonsByName = {}
 
-  const defineKendoButton = (name, icon) => {
+  const defineKendoButton = (name, title, icon) => {
     kendoButtonsByName[name] = {
-      icon: icon || name
+      icon: icon || name,
+      title: title
     }
   }
 
-  defineKendoButton("edit")
-  defineKendoButton("delete")
-  defineKendoButton("search")
-  defineKendoButton("insert", "add")
-  defineKendoButton("update", "check")
-  defineKendoButton("cancelEdit", "cancel")
+  defineKendoButton('edit', 'Edit')
+  defineKendoButton('delete', 'Delete')
+  defineKendoButton('search', 'Search')
+  defineKendoButton('insert', 'Add', 'add')
+  defineKendoButton('update', 'Update', 'check')
+  defineKendoButton('cancelEdit', 'Cancel', 'cancel')
 
   for (const field of Object.keys(jsGrid.ControlField.prototype)) {
-    const i = field.indexOf("ButtonClass")
+    const i = field.indexOf('ButtonClass')
     if (i < 0) {
       continue
     }
@@ -57,6 +62,7 @@
    */
   const default_renderGrid = jsGrid.Grid.prototype._renderGrid
   const default_editRow = jsGrid.Grid.prototype._editRow
+  const default_clearInsert = jsGrid.Grid.prototype.clearInsert
 
   jsGrid.Grid.prototype._renderGrid = function() {
     // Call default jsGrid's rendering
@@ -70,30 +76,43 @@
       if (!_.isFunction(handler)) {
         continue
       }
-      const selector = ".jsgrid-insert-row [name='" + field.name + "']";
+      const selector = '.jsgrid-insert-row [name=' + field.name + ']';
       handler(this._container[0].querySelector(selector), field)
+    }
+  }
+
+  const initDeferredFields = (fields, row, type) => {
+    for (const field of fields) {
+      if (!_.isObject(field.deferred)) {
+        continue
+      }
+      const handler = field.deferred.edit || field.deferred.insert || field.deferred.insertEdit;
+      if (!_.isFunction(handler)) {
+        continue
+      }
+      const selector = '[name=' + field.name + ']';
+      handler(row.querySelector(selector), field)
     }
   }
 
   jsGrid.Grid.prototype._editRow = function($row) {
     // Call default jsGrid's edit row rendering
-    default_editRow.call(this, $row);
+    default_editRow.call(this, $row)
     /*
      * jsGrid hides the row and inserts an editing mode
      * row right after, we need that one.
      */
-    const editRow = $row.prev()[0];
+    const editRow = $row.prev()[0]
     // Now call deferred initializers
-    for (const field of this.fields) {
-      if (!_.isObject(field.deferred)) {
-        continue
-      }
-      const handler = field.deferred.edit || field.deferred.insertEdit;
-      if (!_.isFunction(handler)) {
-        continue
-      }
-      const selector = "[name='" + field.name + "']";
-      handler(editRow.querySelector(selector), field)
-    }
+    initDeferredFields(this.fields, editRow)
+  }
+  /*
+   * JsGrid re-creates the insert row after an insert, so we need to
+   * re-create Kendo any kendo widget present.
+   */
+  jsGrid.Grid.prototype.clearInsert = function() {
+    default_clearInsert.call(this)
+    // Now call deferred initializers
+    initDeferredFields(this.fields, this._insertRow[0])
   }
 })()
