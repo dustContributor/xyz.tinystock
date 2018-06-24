6da3a6048a455a18336cc3f922c5a0e82376186d
dustContributor <hctf90@gmail.com>
2018-06-24 00:32:10 -0300

    - Targeting Java 10 now. - Updated Jooby to 1.4.1 which has Java 9+ related fixes. - Updated to jOOQ 3.11.0, which has Java 9+ support. - Updated pom.xml to override a few project properties (developer, application, etc) to override the ones set in Jooby dependencies.

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
+export JAVA_HOME="/usr/lib/jvm/java-10-openjdk-amd64"
+# jooq needs jaxb module
+export MAVEN_OPTS="--add-modules java.xml.bind"
 mvn jooby:run -Dapplication.debug=5001 -DlogLevel=debug
